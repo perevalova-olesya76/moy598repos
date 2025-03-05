51717357789cdc8b9f8ae90c74aca531f56ab139
alexVinter <160575761+alexVinter@users.noreply.github.com>
2025-03-05 15:08:31 +0700

    Update taskdb.sql

--- taskdb.sql
+++ taskdb.sql
@@ -19,7 +19,7 @@
 -- Table structure for table `tasks`
 --
 
-DROP TABLE IF EXISTS `tasks`;
+DROP TABLE IF EXISTS `zadachi`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `tasks` (
